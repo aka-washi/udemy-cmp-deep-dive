1bd8bb7b9c4588f3919759f3357e15602bcf8219
redeagle <jm06lara@gmail.com>
2026-04-18 03:08:35 +0000

    view content

--- src/app/shared/control/control.component.ts
+++ src/app/shared/control/control.component.ts
@@ -1,4 +1,4 @@
-import { Component, ElementRef, HostBinding, HostListener, inject, input, ViewEncapsulation } from '@angular/core';
+import { Component, ContentChild, ElementRef, HostBinding, HostListener, inject, input, ViewEncapsulation } from '@angular/core';
 
 @Component({
   selector: 'app-control',
@@ -19,9 +19,11 @@ export class ControlComponent {
   // }
   label = input.required<string>();
   private el = inject(ElementRef);
+  @ContentChild('input') private control?: ElementRef<HTMLInputElement | HTMLTextAreaElement>;
 
   onClick() {
     console.log('Clicked control with label: ' + this.label);
     console.log('Element reference:', this.el);
+    console.log('Control reference:', this.control);
   }
 }
