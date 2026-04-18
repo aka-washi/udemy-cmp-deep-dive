3ad40c99ff5479d874f2322589f521293397ee91
redeagle <jm06lara@gmail.com>
2026-04-18 22:45:45 +0000

    after next render

--- src/app/dashboard/tickets/new-ticket/new-ticket.component.ts
+++ src/app/dashboard/tickets/new-ticket/new-ticket.component.ts
@@ -1,4 +1,4 @@
-import { Component, ElementRef, viewChild, ViewChild } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, OnInit, viewChild, ViewChild } from '@angular/core';
 import { ButtonComponent } from '../../../shared/button/button.component';
 import { ControlComponent } from '../../../shared/control/control.component';
 import { FormsModule } from '@angular/forms';
@@ -10,11 +10,21 @@ import { FormsModule } from '@angular/forms';
   templateUrl: './new-ticket.component.html',
   styleUrl: './new-ticket.component.css',
 })
-export class NewTicketComponent {
+export class NewTicketComponent implements OnInit, AfterViewInit {
   //@ViewChild('form') private form?: ElementRef<HTMLFormElement>;
 
   // Signal available in Angular 17.3 or later
   private form = viewChild.required<ElementRef<HTMLFormElement>>('form');
+
+  ngOnInit(): void {
+    console.log('ON INIT');
+    console.log(this.form().nativeElement);
+  }
+
+  ngAfterViewInit() {
+    console.log('AFER VIEW INIT');
+    console.log(this.form().nativeElement);
+  }
 
   // Alternatively to two-way binding, we can also use template reference variables to access the input values on submit.
   onSubmit(titleInput: string, requestInput: string) {
--- src/app/shared/control/control.component.ts
+++ src/app/shared/control/control.component.ts
@@ -1,4 +1,17 @@
-import { Component, contentChild, ContentChild, ElementRef, HostBinding, HostListener, inject, input, ViewEncapsulation } from '@angular/core';
+import {
+  AfterContentInit,
+  afterNextRender,
+  Component,
+  contentChild,
+  ContentChild,
+  ElementRef,
+  HostBinding,
+  HostListener,
+  inject,
+  input,
+  OnInit,
+  ViewEncapsulation,
+} from '@angular/core';
 
 @Component({
   selector: 'app-control',
@@ -11,7 +24,7 @@ import { Component, contentChild, ContentChild, ElementRef, HostBinding, HostLis
     '(click)': 'onClick()'
   }
 })
-export class ControlComponent {
+export class ControlComponent implements OnInit, AfterContentInit {
   // Alternative to using @HostBinding but discouraged as it is less efficient and more verbose
   // @HostBinding('class') className = 'control';
   // @HostListener('click') onClickHost() {
@@ -20,7 +33,29 @@ export class ControlComponent {
   label = input.required<string>();
   private el = inject(ElementRef);
   // @ContentChild('input') private control?: ElementRef<HTMLInputElement | HTMLTextAreaElement>;
-  private control = contentChild.required<ElementRef<HTMLInputElement | HTMLTextAreaElement>>('input');
+  private control =
+    contentChild.required<ElementRef<HTMLInputElement | HTMLTextAreaElement>>(
+      'input',
+    );
+
+  constructor() {
+    console.log('CONSTRUCTOR');
+
+    afterNextRender(() => {
+      console.log('AFTER NEXT RENDER');
+      console.log(this.control());
+    });
+  }
+
+  ngOnInit(): void {
+    console.log('ON INIT');
+    console.log(this.control());
+  }
+
+  ngAfterContentInit(): void {
+    console.log('AFTER CONTENT INIT');
+    console.log(this.control());
+  }
 
   onClick() {
     console.log('Clicked control with label: ' + this.label);
